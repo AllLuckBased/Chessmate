2065a201d554b139758a2421c8c2b0ef209a671e
CHANDRACHUR <chandrachurmukherjee96@gmail.com>
2021-04-10 20:55:33 +0530

    added discord bot for 1v1

--- src/main/java/app/Toolbar.java
+++ src/main/java/app/Toolbar.java
@@ -4,6 +4,7 @@ import components.Settings;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import main.Bot;
 
 public class Toolbar extends MenuBar {
     Display display;
@@ -22,7 +23,15 @@ public class Toolbar extends MenuBar {
         // Game menu items...
         MenuItem analysisBoard = new MenuItem("Analysis Board");
         analysisBoard.setOnAction(e -> display.showAnalysisBoard());
-        gameMenu.getItems().addAll(analysisBoard);
+
+        MenuItem newGame = new MenuItem("New Game");
+        newGame.setOnAction(e -> {
+            Thread discordConnection = new Thread(Bot::run);
+            discordConnection.setDaemon(true);
+            discordConnection.start();
+        });
+
+        gameMenu.getItems().addAll(analysisBoard, newGame);
 
         getMenus().addAll(fileMenu, gameMenu);
     }
